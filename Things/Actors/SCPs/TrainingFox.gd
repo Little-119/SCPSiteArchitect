3753893e2250572907ccaa4de070f9b0d6b36a7e
Little-119 <isaacerwin2000@gmail.com>
2021-04-18 20:25:42 -0400

    Remove cringe

--- Things/Actors/SCPs/TrainingFox.gd
+++ Things/Actors/SCPs/TrainingFox.gd
@@ -8,26 +8,3 @@ func _init().():
 	color = Color.orange
 	has_fine_manipulation = false
 	drives.append("Wander")
-	needs.append(HeadpatHunger)
-
-class NeedHeadpat extends "res://AI/Jobs/InteractWith.gd":
-	func _init():
-		print("headpat pls")
-		name = "NeedHeadpat"
-	
-	func on_done(actor):
-		get_parent().needs_dict["HeadpatHonger"].honger = 0
-		queue_free()
-
-class HeadpatHunger extends Need:
-	var honger = 20
-	var job = null
-	func _init():
-		type = "HeadpatHonger"
-	func on_life_process():
-		honger += 1
-	func on_ai_process():
-		if honger > 50:
-			job = actor.emit_job(NeedHeadpat)
-		elif job:
-			job.queue_free()
