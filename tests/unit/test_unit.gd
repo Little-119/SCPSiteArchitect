9618e83d2178c47ab0fcdf25e16bf7834e8d159d
Little-119 <positronic.june@protonmail.com>
2021-06-23 01:46:53 -0400

    Remove test for unimplemented feature

--- tests/unit/test_unit.gd
+++ tests/unit/test_unit.gd
@@ -4,9 +4,3 @@ class TestMap extends "res://addons/gut/test.gd": # tests for in-game date funct
 	var map = null
 	func before_all():
 		map = autofree(Map.new(Vector3.ZERO))
-	
-	func test_thirtysec():
-		assert_eq(map.get_local_time(30).seconds,30 * Constants.turn_length)
-	
-	#func test_oneandhalfminute():
-		#assert_eq(map.get_local_time(130 * Constants.turn_length).seconds,10)
