2fd3d7ab5398bbb0493c9367e71c5d42cb6448f3
Little-119 <positronic.june@protonmail.com>
2021-06-24 07:21:53 -0400

    Add neat ASCII percentage bar generator

--- Globals.gd
+++ Globals.gd
@@ -1,5 +1,30 @@
 extends Node
 # warning-ignore-all:unused_class_variable
-# Contains global variables to be accessed by other scripts
+# Contains global variables and functions to be accessed by other scripts
 
 var god_mode: bool = false # god mode allows instant (de)construction. Here so Structures can access it before entering the tree
+
+const BAR_FOR_TEST = "01234" # GUT can't render special characters in its output apparently
+const BAR_BLOCK_FADING_STYLE = "█▓▒░_"
+const BAR_BLOCK_STYLE = "▉▊▌▎ "
+const BAR_CDDA_STYLE = "|\\\\\\."
+
+static func generate_ascii_progress_bar(percent: float,size: int,style: String = BAR_BLOCK_FADING_STYLE) -> String:
+	percent = round(percent)
+	var to_fill = size * (percent / 100)
+	var remainder = fmod(to_fill,1)
+	var middle_char: String
+	match (remainder * 10) as int:
+		1,2,3:
+			middle_char = style[3]
+		4,5,6:
+			middle_char = style[2]
+		7,8,9:
+			middle_char = style[1]
+	
+	var front: String = style[0].repeat(floor(to_fill)) if floor(to_fill) > 0 else ""
+	var to_fill_r: int = size - (front.length() + middle_char.length())
+	var back: String = style[4].repeat(to_fill_r) if to_fill_r > 0 else ""
+	var string: String = front + middle_char + back
+	if string.length() != size: breakpoint
+	return string
--- tests/unit/test_unit.gd
+++ tests/unit/test_unit.gd
@@ -4,3 +4,29 @@ class TestMap extends "res://addons/gut/test.gd":
 	var map = null
 	func before_all():
 		map = autofree(Map.new(Vector3.ZERO))
+
+class TestStringGenerators extends "res://addons/gut/test.gd":
+	var style = Globals.BAR_FOR_TEST
+	func test_generate_ascii_bar_full():
+		var string = Globals.generate_ascii_progress_bar(100,10,style)
+		assert_eq(string,"0".repeat(10))
+	
+	func test_generate_ascii_bar_half():
+		var string = Globals.generate_ascii_progress_bar(50,10,style)
+		assert_eq(string,"0000044444")
+	
+	func test_generate_ascii_bar_half_with_odd_size():
+		var string = Globals.generate_ascii_progress_bar(50,11,style)
+		assert_eq(string,"00000244444")
+	
+	func test_generate_ascii_bar_almost_full():
+		var string = Globals.generate_ascii_progress_bar(99,10,style)
+		assert_eq(string,"0".repeat(9) + "1")
+	
+	func test_generate_ascii_bar_almost_empty():
+		var string = Globals.generate_ascii_progress_bar(1,10,style)
+		assert_eq(string,"3" + "4".repeat(9))
+	
+	func test_generate_ascii_bar_all_numbers(param=use_parameters(range(0,100))):
+		var string = Globals.generate_ascii_progress_bar(param,10,style)
+		assert_eq(string.length(),10)
