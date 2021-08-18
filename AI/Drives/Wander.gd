f8a81482b54c65a73249942a519f348707e0c950
Little-119 <positronic.june@protonmail.com>
2021-08-18 18:40:57 -0400

    Improve AI\Drives\Wander performance
    ...hopefully
    Array.shuffle is probably more expensive than I initially assumed

--- AI/Drives/Wander.gd
+++ AI/Drives/Wander.gd
@@ -1,23 +1,32 @@
 extends "Drive.gd"
 
 var moveto
+var destination: Cell = null
 
 func _init():
 	type = "wander"
 	display_name = "Wandering"
+
+func act_async(_userdata):
+	if not moveto and not destination:
+		var destinations: Array = actor.cell.get_cells_in_radius(3)
+		var destination: Cell = null
+		while not destinations.empty():
+			# randomly pick a cell, check if it's reachable. If not, pick another one, repeat
+			var i: int = (randi() % destinations.size())
+			var cell: Cell = destinations[i]
+			if not actor.astar.test_path_to(cell):
+				destinations.remove(i)
+				continue
+			else:
+				destination = cell
+				break
 
 func act():
 	.act()
 	if not moveto in actor.actions:
 		moveto = null
 	if not moveto:
-		var destinations: Array = actor.cell.get_cells_in_radius(3)
-		destinations.shuffle()
-		var destination: Cell = null
-		for cell in destinations:
-			if actor.astar.test_path_to(cell):
-				#destination = cell
-				destination = actor.cell.get_adjacent_cell(Vector3(0,-2,0))
-				break
 		if destination:
 			moveto = actor.do_action("MoveTo",destination,self)
+			destination = null
